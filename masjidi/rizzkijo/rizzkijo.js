8d81501f3ae898541686bbdce7d68c3de29e1995
Rizki Johan <rj.workinghard@gmail.com>
2020-12-29 22:17:56 +0700

    template masjidi

---
+++ masjidi/rizzkijo/rizzkijo.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	$("#edomlain").click(function(){           
+	    $('#edomlain').html($('#edomlain').text() == 'Sembunyikan' ? 'Lainnya' : 'Sembunyikan');
+	    $('#edomlain-box > .uil').toggleClass('rotate');
+	    $('.dompet-box').toggleClass('expand');
+	});
+
+	$("#backLink").click(function(){           
+	    window.history.back();
+	});
+
+	$("#search-campaign").click(function(){           
+	    $('.search-box').toggleClass('d-none');
+	    $('.uil-cari').toggleClass('d-none');
+	    $('.uil-times').toggleClass('d-none');
+	});
+})
+$(window).scroll(function(){
+	$('#detailProgramBar').toggleClass('shrink', $(this).scrollTop() > 56);
+});
